4179a47989ed8ef7211ee9622c7b17989b33cfee
MekanEs <Mekan.es1997@gmail.com>
2023-08-04 23:42:41 +0300

    feat: add basic routing add routing with basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FunctionComponent } from 'react';
 import './App.scss';
+import { Routes, Route, BrowserRouter, NavLink } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route index path="/" element={<Main />} />
+          <Route path="about" element={<About />} />
+          <Route path="registration" element={<Registartion />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+        <footer className="App-footer">Footer</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
+// eslint-disable-next-line max-lines-per-function
+const Header: FunctionComponent = () => {
+  return (
+    <header className="App-header">
+      <ul className="nav">
+        <li>
+          <button>
+            <NavLink to="/">Main</NavLink>
+          </button>
+        </li>
+        <li>
+          <button>
+            <NavLink to="about">About</NavLink>
+          </button>
+        </li>
+        <li>
+          <button>
+            <NavLink to="registration">Registartion</NavLink>
+          </button>
+        </li>
+        <li>
+          <button>
+            <NavLink to="login">Login</NavLink>
+          </button>
+        </li>
+      </ul>
+    </header>
+  );
+};
+const Main: FunctionComponent = () => {
+  return <div>Main</div>;
+};
+const Registartion: FunctionComponent = () => {
+  return <div>Registartion</div>;
+};
+const Login: FunctionComponent = () => {
+  return <div>Login</div>;
+};
+const About: FunctionComponent = () => {
+  return <div>About</div>;
+};
+const PageNotFound: FunctionComponent = () => {
+  return <div>404 Page not found</div>;
+};
 export default App;
