b87c67d4fc9425e34432ff9a4414cfb29a738b13
MekanEs <Mekan.es1997@gmail.com>
2023-08-06 13:49:40 +0300

    refactor: add structure bring the components to the correct structure, add module.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React, { FunctionComponent } from 'react';
-import './App.scss';
-import { Routes, Route, BrowserRouter, NavLink } from 'react-router-dom';
+import React from 'react';
+import styles from './App.module.scss';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { About, Login, Main, PageNotFound, Registartion } from './pages';
+import { Footer, Header } from './Components';
 
-function App(): JSX.Element {
+const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className={styles.App}>
         <Header />
         <Routes>
           <Route index path="/" element={<Main />} />
@@ -14,54 +16,10 @@ function App(): JSX.Element {
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
-        <footer className="App-footer">Footer</footer>
+        <Footer />
       </div>
     </BrowserRouter>
   );
-}
+};
 
-// eslint-disable-next-line max-lines-per-function
-const Header: FunctionComponent = () => {
-  return (
-    <header className="App-header">
-      <ul className="nav">
-        <li>
-          <button>
-            <NavLink to="/">Main</NavLink>
-          </button>
-        </li>
-        <li>
-          <button>
-            <NavLink to="about">About</NavLink>
-          </button>
-        </li>
-        <li>
-          <button>
-            <NavLink to="registration">Registartion</NavLink>
-          </button>
-        </li>
-        <li>
-          <button>
-            <NavLink to="login">Login</NavLink>
-          </button>
-        </li>
-      </ul>
-    </header>
-  );
-};
-const Main: FunctionComponent = () => {
-  return <div>Main</div>;
-};
-const Registartion: FunctionComponent = () => {
-  return <div>Registartion</div>;
-};
-const Login: FunctionComponent = () => {
-  return <div>Login</div>;
-};
-const About: FunctionComponent = () => {
-  return <div>About</div>;
-};
-const PageNotFound: FunctionComponent = () => {
-  return <div>404 Page not found</div>;
-};
 export default App;
---
+++ src/Components/footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Footer.module.scss';
+const Footer: React.FC = () => {
+  return <footer className={styles.footer}>Footer</footer>;
+};
+
+export default Footer;
