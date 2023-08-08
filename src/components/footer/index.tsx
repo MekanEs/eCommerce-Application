a618b8abbdc219144f2d3603d9e90c4024376370
MekanEs <Mekan.es1997@gmail.com>
2023-08-08 15:48:41 +0300

    chore: rename files

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './footer.module.scss';
+import styles from './ooter.module.scss';
 const Footer: React.FC = () => {
   return <footer className={styles.footer}>Footer</footer>;
 };
