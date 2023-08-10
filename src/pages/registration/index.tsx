08b00f47bf93b2cac5c3bda144844aa640b6f094
Liza <isupova.97@mail.ru>
2023-08-10 13:54:36 +0300

    feat: add a link to go to the login page

--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import styles from './registration.module.scss';
+import { Link } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { FormFields } from '../../utils/helpers/interface';
 
-const Registartion: React.FC = () => {
-  return <div>Registartion</div>;
+const Registartion: React.FC = (): JSX.Element => {
+  const { handleSubmit, reset } = useForm<FormFields>({ mode: 'onChange' });
+  const onSubmit: SubmitHandler<FormFields> = () => reset();
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles['page-title']}>Welcome to «Veros» Store</h2>
+      <div className={styles['login-link']}>
+        <p>Have an account? </p>
+        <Link to="/login">Log in</Link>
+      </div>
+      <div className={styles['form-container']}>
+        <form
+          className={styles['login-form']}
+          onSubmit={handleSubmit(onSubmit)}
+        ></form>
+      </div>
+    </div>
+  );
 };
+
 export default Registartion;
