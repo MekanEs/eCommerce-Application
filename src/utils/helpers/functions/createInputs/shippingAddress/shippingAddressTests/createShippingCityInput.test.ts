62fc3ca52d697be0b6eb1c855f2b7a00002bcead
Liza <isupova.97@mail.ru>
2023-08-19 18:14:49 +0300

    feat: add tests for login and registration page functions

---
+++ src/utils/helpers/functions/createInputs/shippingAddress/shippingAddressTests/createShippingCityInput.test.ts
@@ -0,0 +1,76 @@
+import { render, renderHook, screen } from '@testing-library/react';
+import { useForm, UseFormReturn } from 'react-hook-form';
+import createShippingCityInput from '../createShippingCityInput';
+import { FormFields } from '../../../../../../interfaces/formInputs';
+
+test('renders shipping city input without errors', () => {
+  const { result } = renderHook(() =>
+    useForm<FormFields>({
+      mode: 'onChange',
+    }),
+  );
+
+  const formMock: UseFormReturn<FormFields> = {
+    ...result.current,
+  };
+
+  render(createShippingCityInput(formMock));
+
+  const shippingCityInput = screen.getByLabelText('City');
+  expect(shippingCityInput).toBeInTheDocument();
+});
+
+test('displays error message when shipping city is required', () => {
+  const { result } = renderHook(() =>
+    useForm<FormFields>({
+      mode: 'onChange',
+    }),
+  );
+
+  const formMock: UseFormReturn<FormFields> = {
+    ...result.current,
+    formState: {
+      ...result.current.formState,
+      errors: {
+        ...result.current.formState.errors,
+        shippingCity: {
+          type: 'manual',
+          message: 'The field is required',
+        },
+      },
+    },
+  };
+
+  render(createShippingCityInput(formMock));
+
+  const errorMessage = screen.getByText('The field is required');
+  expect(errorMessage).toBeInTheDocument();
+});
+
+test('does not display error message when shipping city is valid', () => {
+  const { result } = renderHook(() =>
+    useForm<FormFields>({
+      mode: 'onChange',
+    }),
+  );
+
+  const formMock: UseFormReturn<FormFields> = {
+    ...result.current,
+    formState: {
+      ...result.current.formState,
+      dirtyFields: {
+        ...result.current.formState.dirtyFields,
+        shippingCity: true,
+      },
+      errors: {
+        ...result.current.formState.errors,
+        shippingCity: undefined,
+      },
+    },
+  };
+
+  render(createShippingCityInput(formMock));
+
+  const errorMessage = screen.queryByText('The field is required');
+  expect(errorMessage).toBeNull();
+});
---
+++ src/utils/helpers/functions/validate/validateTests/validateDefault.test.ts
@@ -0,0 +1,27 @@
+import validateName from '../validateDefault';
+
+describe('validateName', () => {
+  test('returns an error message when value is empty', () => {
+    const value = '';
+    const result = validateName(value);
+    expect(result).toBe('Value must contain at least one character');
+  });
+
+  test('returns an error message when value contains special characters', () => {
+    const value = 'John!Doe';
+    const result = validateName(value);
+    expect(result).toBe('Value must not contain special characters');
+  });
+
+  test('returns an error message when value contains digits', () => {
+    const value = 'John123';
+    const result = validateName(value);
+    expect(result).toBe('Value must not contain digits');
+  });
+
+  test('returns true when value is valid', () => {
+    const value = 'John Doe';
+    const result = validateName(value);
+    expect(result).toBe(true);
+  });
+});
