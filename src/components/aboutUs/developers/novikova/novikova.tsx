c8fd224a1c22678e46e822e695c99c24897ff2d1
Liza <isupova.97@mail.ru>
2023-09-10 23:37:09 +0300

    feat: add tests and information about a team member: Esenjanow

--- src/components/aboutUs/developers/novikova/novikova.tsx
+++ src/components/aboutUs/developers/novikova/novikova.tsx
@@ -1,5 +1,5 @@
 import styles from '../developer.module.scss';
-import developerPhoto from '../../../../assets/img/png/novikova.png';
+import developerPhoto from '../../../../assets/img/png/liza.png';
 
 const DeveloperNovikova = (): JSX.Element => {
   return (
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './about.module.scss';
 import AboutGeneralInfo from '../../components/aboutUs/general/generalInfo';
 import DeveloperNovikova from '../../components/aboutUs/developers/novikova/novikova';
+import DeveloperEsenjanow from '../../components/aboutUs/developers/esenjanow/esenjanow';
 
 const About: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const About: React.FC = () => {
       <h2 className={styles['page-title']}>our team</h2>
       <AboutGeneralInfo />
       <DeveloperNovikova />
+      <DeveloperEsenjanow />
     </div>
   );
 };
