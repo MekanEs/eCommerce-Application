7a68393dc8c1c3e28045805b6a254e16b3c13b14
sergpet93 <maknev96@mail.ru>
2023-09-13 16:34:46 +0300

    feat: implement carts skeleton in catalog

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -7,12 +7,14 @@ import { hasChildren } from '../../utils/helpers/catalogPage/hasChildrenCategory
 import SubCategories from './subCategories';
 import CategoryButton from './categoryButton';
 import BreadCrumbs from './breadCrumbs';
+import { SkeletonHeader } from '../skeleton';
 
 const Categories: React.FC = () => {
   const dispatch = useAppDispatch();
   const categories = useAppSelector((state) => state.catalog.categories);
   const childCategory = useAppSelector((state) => state.catalog.childCategory);
   const activeCategory = useAppSelector((state) => state.filter.category);
+  const [isLoading, setIsLoading] = useState(true);
   const [activeAncestor, setActiveAncestor] = useState<categoryType | null>(
     null,
   );
@@ -24,7 +26,11 @@ const Categories: React.FC = () => {
     ) {
       setActiveAncestor(activeCategory);
     }
+    if (categories && categories.length > 0) {
+      setIsLoading(false);
+    }
   });
+
   const handleClick = (el: categoryType): void => {
     dispatch(setActiveCategory(el));
   };
@@ -32,7 +38,10 @@ const Categories: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.categories}>
-        {categories &&
+        {isLoading ? (
+          <SkeletonHeader />
+        ) : (
+          categories &&
           categories.map((el, index) => (
             <div key={index}>
               <CategoryButton
@@ -43,7 +52,8 @@ const Categories: React.FC = () => {
                 callback={handleClick}
               />
             </div>
-          ))}
+          ))
+        )}
       </div>
       <div className={styles.categories}>
         {activeAncestor
--- src/components/skeleton/index.tsx
+++ src/components/skeleton/index.tsx
@@ -1,18 +1,6 @@
 import ContentLoader from 'react-content-loader';
 
-interface SkeletonTextProps {
-  variant: 'text';
-}
-
-interface SkeletonShapeProps {
-  variant: 'rect' | 'circle';
-  width: number;
-  height: number;
-}
-
-type SkeletonProps = SkeletonTextProps | SkeletonShapeProps;
-
-const Skeleton = (props: SkeletonProps) => (
+const Skeleton = (): JSX.Element => (
   <ContentLoader
     speed={2}
     width={290}
@@ -20,17 +8,29 @@ const Skeleton = (props: SkeletonProps) => (
     viewBox="0 0 290 500"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
-    {...props}
   >
-    <rect x="0" y="0" rx="0" ry="0" width="4" height="500" />
-    <rect x="0" y="0" rx="0" ry="0" width="290" height="4" />
-    <rect x="286" y="0" rx="0" ry="0" width="4" height="500" />
-    <rect x="0" y="496" rx="0" ry="0" width="290" height="4" />
-    <rect x="29" y="14" rx="0" ry="0" width="232" height="40" />
-    <rect x="10" y="64" rx="0" ry="0" width="270" height="171" />
-    <rect x="55" y="245" rx="0" ry="0" width="180" height="105" />
-    <rect x="15" y="366" rx="0" ry="0" width="260" height="48" />
-    <rect x="15" y="426" rx="0" ry="0" width="260" height="48" />
+    <rect x="0" y="0" rx="3" ry="3" width="4" height="500" />
+    <rect x="0" y="0" rx="3" ry="3" width="290" height="4" />
+    <rect x="286" y="0" rx="3" ry="3" width="4" height="500" />
+    <rect x="0" y="496" rx="3" ry="3" width="290" height="4" />
+    <rect x="29" y="14" rx="3" ry="3" width="232" height="40" />
+    <rect x="10" y="64" rx="3" ry="3" width="270" height="171" />
+    <rect x="55" y="245" rx="3" ry="3" width="180" height="105" />
+    <rect x="15" y="366" rx="3" ry="3" width="260" height="48" />
+    <rect x="15" y="426" rx="3" ry="3" width="260" height="48" />
+  </ContentLoader>
+);
+
+export const SkeletonHeader = (): JSX.Element => (
+  <ContentLoader
+    speed={2}
+    width={600}
+    height={24}
+    viewBox="0 0 600 24"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="0" rx="3" ry="3" width="600" height="24" />
   </ContentLoader>
 );
 
