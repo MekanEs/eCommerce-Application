8440402b9e6c7823c8b79b9424805e471ed23ec8
sergpet93 <maknev96@mail.ru>
2023-09-15 12:25:23 +0300

    feat: add button checkout in basket

--- src/components/orderInfo/index.tsx
+++ src/components/orderInfo/index.tsx
@@ -4,6 +4,7 @@ import styles from './orderInfo.module.scss';
 import ClearCart from './clearCart';
 import CartDiscount from './discount';
 import OrderDetails from './orderDetails';
+import CheckoutCart from './checkout';
 
 type OrderInfoPropsType = {
   cart: Cart | undefined;
@@ -18,6 +19,7 @@ const OrderInfo: React.FC<OrderInfoPropsType> = ({ cart }) => {
         <OrderDetails cart={cart} />
 
         <CartDiscount cart={cart} />
+        <CheckoutCart basket={cart} />
       </div>
 
       <ClearCart basket={cart} />
